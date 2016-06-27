b68983df0c1ea6e1c34b6911711eac492fb8afed
zhangwj <zhangweijian@jd.com>
2016-06-27 14:40:37 +0800

    test zipWithIndex

--- src/main/java/zwj/test105_Function2/testFunction.scala
+++ src/main/java/zwj/test105_Function2/testFunction.scala
@@ -50,7 +50,8 @@ object testFunction {
     "Mary has a little lamb".split(" ").sortWith( _.length < _.length).foreach(println)
 
     //zipWithIndex函数将元素和其所在的下标组成一个pair
-    println((1 to 9).zipWithIndex)  //Vector((1,0), (2,1), (3,2), (4,3), (5,4), (6,5), (7,6), (8,7), (9,8))
+    println((1 to 9).zipWithIndex)  //index加到了第二个位置，Vector((1,0), (2,1), (3,2), (4,3), (5,4), (6,5), (7,6), (8,7), (9,8))
+    println((11 to 19).zipWithIndex.map(_.swap))  //Vector((0,11), (1,12), (2,13), (3,14), (4,15), (5,16), (6,17), (7,18), (8,19))
   }
 
   def main(args:Array[String]): Unit ={
